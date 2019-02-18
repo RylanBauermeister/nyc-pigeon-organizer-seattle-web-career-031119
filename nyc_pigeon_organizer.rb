798dc7fc2d1382ab83a5cb5833ddd3cb576c0284
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-18 01:53:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,13 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = Hash.new
-    binding.pry
+   
   data.each do |attribute, subcategory|
+     binding.pry
     subcategory.each do |category, pigeons|
+       binding.pry
       pigeons.each do |name|
+         binding.pry
         if result.include?(name)
           if result[name].include?(attribute)
             result[name][attribute] << category
