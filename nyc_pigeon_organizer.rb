7f4202907a975f6966291cf510c51ebfd6cb0dc0
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-18 01:47:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |attribute, subcategory|
+    subcategory.each do |category, pigeons|
+      pigeons.each do |name|
+        if result.inlude?(name)
+          if result[name].include?(attribute)
+            result[name][attribute] << 
+  
+  
+  
 end
