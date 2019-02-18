f1a76a69bd1329519fe6cd2b64dbd7a8fad14025
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-18 01:51:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
@@ -16,5 +18,6 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
+  binding.pry
   result
 end
